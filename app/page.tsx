6296a83953fa74db9438c0d6d519ff2a894f45fa
ajarek2101 <ajarek2101@gmail.com>
2024-10-02 22:36:17 +0000

    added carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Header from '@/components/Header'
+import TrendingRecipes from '@/components/TrendingRecipes'
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-24 max-sm:px-4">
+    <main className="flex min-h-screen flex-col items-center justify-between px-24 pb-12 max-sm:px-4 gap-4">
       <Header />
+      <TrendingRecipes/>
     </main>
   )
 }
---
+++ components/TrendingRecipes.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Card, CardContent, CardFooter } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import recipes from '@/data/data.json'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const TrendingRecipes = () => {
+  return (
+    <div className='flex flex-col items-center justify-center mt-4 gap-4 '>
+      <h1 className='text-2xl font-semibold'>Trending <span className='text-primary'>Recipes</span></h1>
+        <Carousel className="w-full max-w-5xl max-sm:max-w-sm max-md:max-w-md max-lg:max-w-lg">
+      <CarouselContent className="-ml-1">
+        {recipes.map((recipe, index) => (
+           <CarouselItem key={index} className="pl-1 sm:basis-1/2 md:basis-1/3 lg:basis-1/5">
+           <div className="p-1">
+             <Card>
+              <Link href={`/recipe/${recipe.id}`}>
+
+               <CardContent className="relative flex aspect-square items-center justify-center p-6">
+               <Image
+          src={recipe.image}
+          alt="recipe"
+         fill
+          className="rounded-xl object-cover "
+          priority
+        />
+               </CardContent>
+               <CardFooter className="h-20 p-1 text-primary">{recipe.name}</CardFooter>
+               </Link>
+             </Card>
+           </div>
+         </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+    </div>
+  )
+}
+
+export default TrendingRecipes
